708f1650981f460b2d10b28373939a6ef84e82da
fmotac <fmotac@gmail.com>
2024-11-01 07:49:27 -0300

    etec

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Olá, Azure! Nós somos da Recode');
+  res.send('Olá, Azure! Nós somos da Recode 2024');
 });
 
 app.listen(PORT, () => {
